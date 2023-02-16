a70dd8e25e49f5af78de1b8ed299f16434b3a8a4
benbroce <benbroce@byu.edu>
2023-02-16 01:59:57 -0700

    Add timerDisplay module

---
+++ timerDisplay.cpp
@@ -0,0 +1,58 @@
+// Author: Ben Broce (BYU ECEn ELC)
+
+#include "timerDisplay.h"
+#include "pinDefs.h"
+#include <SPI.h>
+
+// 7-segment control bytes for decimal digits
+// ex: SEG_DIGIT[0] gives byte to write to shift register to display "0"
+const uint8_t displayDigit[10] = {
+    0x7E, // "0" - 01111110
+    0x0C, // "1" - 00001100
+    0xB6, // "2" - 10110110
+    0x9E, // "3" - 10011110
+    0xCC, // "4" - 11001100
+    0xDA, // "5" - 11011010
+    0xFA, // "6" - 11111010
+    0x0E, // "7" - 00001110
+    0xFE, // "8" - 11111110
+    0xDE  // "9" - 11011110
+};
+
+// initialize the display hardware (call before writes)
+void displayInit() {
+  // release register values -> 7-seg output
+  pinMode(SHIFTREG_RCK_PIN, OUTPUT);
+  digitalWrite(SHIFTREG_RCK_PIN, HIGH);
+  // init SPI hardware
+  // (set max data rate, MSB first, and
+  // SPI_MODE1: clock idles low, bit write on rising clk)
+  SPI.beginTransaction(
+      SPISettings(DISPLAY_SHIFTREG_MAX_RATE, MSBFIRST, SPI_MODE1));
+}
+
+// write bytes to each display's shift register
+void displayWriteBytes(uint8_t left, uint8_t middle, uint8_t right) {
+  // hold 7-seg output while writing data
+  digitalWrite(SHIFTREG_RCK_PIN, LOW);
+
+  // write bytes in order: left, middle, right digit
+  SPI.transfer(left);
+  SPI.transfer(middle);
+  SPI.transfer(right);
+
+  // release reg values -> 7-seg to display new values
+  digitalWrite(SHIFTREG_RCK_PIN, HIGH);
+}
+
+// write a time to the display in the format "M.SS"
+void displayWriteTime(uint8_t min, uint8_t tenSec, uint8_t oneSec) {
+  displayWriteBytes((displayDigit[min] | DISPLAY_DIGIT_DP_MASK),
+                    displayDigit[tenSec], displayDigit[oneSec]);
+}
+
+// turn off all display segments
+void displayClear() {
+  displayWriteBytes(DISPLAY_DIGIT_CLR_MASK, DISPLAY_DIGIT_CLR_MASK,
+                    DISPLAY_DIGIT_CLR_MASK);
+}
---
+++ timerDisplay.h
@@ -0,0 +1,48 @@
+// Author: Ben Broce (BYU ECEn ELC)
+
+// Display module for driving large 7-segment displays over Arduino SPI bus via
+// TPIC6B595 shift registers
+
+//////////////////////////////
+// 7-Segment Layout
+// - number label is the output pin of the shift register,
+//   and the index in the byte to write to the shift register
+// - NOTE: a 1 turns the segment on (sink), a 0 turns it off (hi-Z)
+//
+//   _(1)_
+//  |     |
+// (6)   (2)
+//  |     |
+//   _(7)_
+//  |     |
+// (5)   (3)
+//  |     |
+//   _(4)_  .(0)
+//////////////////////////////
+
+#ifndef TIMER_DISPLAY
+#define TIMER_DISPLAY
+
+#include <stdint.h>
+
+#define DISPLAY_SHIFTREG_MAX_RATE 20000000 // max shift rate in Hz
+#define DISPLAY_DIGIT_DP_MASK 0x01  // "." - 0000001 (right of digit with mask)
+#define DISPLAY_DIGIT_CLR_MASK 0x00 // "" - 0000000 (clear byte)
+
+// 7-segment control bytes for decimal digits
+// ex: displayDigit[0] gives byte to write to shift register to display "0"
+extern const uint8_t displayDigit[10];
+
+// initialize the display hardware (call before writes)
+void displayInit();
+
+// write a time to the display in the format "M.SS"
+void displayWriteTime(uint8_t min, uint8_t tenSec, uint8_t oneSec);
+
+// write bytes to each display's shift register
+void displayWriteBytes(uint8_t left, uint8_t middle, uint8_t right);
+
+// turn off all display segments
+void displayClear();
+
+#endif // TIMER_DISPLAY
